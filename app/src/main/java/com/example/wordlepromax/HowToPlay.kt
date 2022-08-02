da7e976502b0497c4c70e5abf3ffa29f09219be9
surya <balasurya811@gmail.com>
2022-08-02 20:26:08 +0530

    Migrations and Bug Fixes
    Migrates majority Java source files to Kotlin and fixes some bugs in compareWords

---
+++ app/src/main/java/com/example/wordlepromax/HowToPlay.kt
@@ -0,0 +1,19 @@
+package com.example.wordlepromax
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+
+class HowToPlay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> setContentView(R.layout.how_to_play_dark)
+            Configuration.UI_MODE_NIGHT_NO -> setContentView(R.layout.how_to_play)
+        }
+        val toolBar = findViewById<Toolbar>(R.id.my_toolbar)
+        setSupportActionBar(toolBar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+}
---
+++ app/src/main/java/com/example/wordlepromax/Key.kt
@@ -0,0 +1,9 @@
+package com.example.wordlepromax
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatButton
+
+class Key(context: Context?, attrs: AttributeSet?) : AppCompatButton(
+    context!!, attrs
+)
